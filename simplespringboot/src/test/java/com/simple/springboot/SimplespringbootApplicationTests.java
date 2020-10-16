bc93ab3d44bd0d4cbbfea62173d051bc832776ed
default <lipika.jk@gmail.com>
2020-10-16 08:44:31 +0200

    Initial commit

---
+++ simplespringboot/src/test/java/com/simple/springboot/SimplespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
